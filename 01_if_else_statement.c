62f8d874ce637c28eba20baa9d17f8d05c67dbdb
Pushkar Raj <pushkar.1974.78@gmail.com>
2026-09-03 23:52:47 +0530

    Create 01_if_else_statement.c

---
+++ 01_if_else_statement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+     int a = 5;
+     if (a > 6)
+     {
+            printf("a is greater than 6\n");
+        }
+        else
+        {
+            printf("a is not greater than 6\n");
+     }
+    return 0;
+}
